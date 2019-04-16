6daa38f1590ce35e23f3c69c10ffc111ead05135
Kenneth Illman <kenneth.illman@gmail.com>
2019-04-16 10:01:32 +0530

    small footer update

--- wp-content/themes/voytalent/footer.php
+++ wp-content/themes/voytalent/footer.php
@@ -1,19 +1,14 @@
-<?php
-/**
- * The template for displaying the footer
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package WordPress
- * @subpackage Twenty_Nineteen
- * @since 1.0.0
- */
 
-?>
 
-Site Footer Content Here
+<section class="s-footer">
+	<div class="gc">
+		<div class="g-12">
+			footer
+		</div>
+	</div>
+</section>
+
+
 <?php wp_footer(); ?>
 
 </body>
