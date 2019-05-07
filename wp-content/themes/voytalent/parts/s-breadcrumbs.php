a70380acba838b774979cf4736c92b2d2a9dff52
Kenneth Illman <kenneth.illman@gmail.com>
2019-05-07 09:55:36 +0530

    removed unused code

--- wp-content/themes/voytalent/parts/s-breadcrumbs.php
+++ wp-content/themes/voytalent/parts/s-breadcrumbs.php
@@ -11,9 +11,9 @@
 ?>
 
 <section class="s-breadcrumbs">
+
   <div class="body">
       <div class="gc">
-        <!--<div class="g-12"><a href="">Voy</a><a href="">Jobs</a>Creative director</div>-->
         <div class="g-12">
             <?php
                 if (!is_home() && !is_front_page() && function_exists('yoast_breadcrumb')) {
