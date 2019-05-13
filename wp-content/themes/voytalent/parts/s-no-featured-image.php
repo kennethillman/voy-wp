23a665aac52579b263782bee62023a463108e213
Kenneth Illman <kenneth.illman@gmail.com>
2019-05-13 10:04:44 +0530

    Styling for headers when no featured image present,

--- wp-content/themes/voytalent/parts/s-no-featured-image.php
+++ wp-content/themes/voytalent/parts/s-no-featured-image.php
@@ -3,17 +3,26 @@
     if(get_the_post_thumbnail_url(get_the_ID()) == ''):
 ?>
 
-    <div class="headers">
-        <?php if(!empty($theTitle)) : ?>
-            <h2 class="header">
-                <?php echo $theTitle;?>
-            </h2>
-        <?php  endif; ?>
-        <?php if(!empty($theSubTitle)) : ?>
-            <h3 class="sub-header">
-                <?php echo $theSubTitle;?>
-            </h3>
-        <?php  endif; ?>
+
+  <section class="s-no-featured-image">
+    <div class="gc">
+      <div class="g-12" >
+
+        <div class="headers">
+            <?php if(!empty($theTitle)) : ?>
+                <h2 class="header">
+                    <?php echo $theTitle;?>
+                </h2><br>
+            <?php  endif; ?>
+            <?php if(!empty($theSubTitle)) : ?>
+                <h3 class="sub-header">
+                    <?php echo $theSubTitle;?>
+                </h3>
+            <?php  endif; ?>
+        </div>
+
+      </div>
     </div>
+  </section>
 
 <?php  endif; ?>
