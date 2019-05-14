302258fbb3b25fce9aedf7f336633af1056f8a17
Kenneth Illman <kenneth.illman@gmail.com>
2019-05-14 15:01:00 +0530

    added link to specific job

--- wp-content/themes/voytalent/parts/s-join-email.php
+++ wp-content/themes/voytalent/parts/s-join-email.php
@@ -10,14 +10,17 @@
 
 ?>
 
+<?php
+  $getJoinLink = get_site_url() . '/jobs/job-details/?jid=DF3F17BC4C';
+?>
 
 <section class="s-join-email">
   <div class="gc -g-shift-vp600">
     <div class="g-9 g-m-12">
-      Sign up and recive the latest announcements, tips, networking invitations and more
+      Sign up and recive the latest announcements, tips, networking invitations and more.
     </div>
     <div class="g-3 g-m-12">
-      <a class="btn -black -block">Join Voy</a>
+      <a href="<?php echo $getJoinLink ?>" class="btn -black -block">Join Voy</a>
     </div>
   </div>
 </section>
