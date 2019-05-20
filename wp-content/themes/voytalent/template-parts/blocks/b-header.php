f74198dffb640d2c0a2a915a64089f16527d6649
Kavitha <kavitha.cm@indpro.se>
2019-05-20 14:50:13 +0530

    Voy -header block

---
+++ wp-content/themes/voytalent/template-parts/blocks/b-header.php
@@ -0,0 +1,27 @@
+<?php
+    $id = $block['id'];
+    if( !empty($block['anchor']) ) {
+        $id = $block['anchor'];
+    }
+
+    $className = '';
+    if( !empty($block['className']) ) {
+        $className .= ' ' . $block['className'];
+    }
+    if( !empty($block['align']) ) {
+        $className .= ' align' . $block['align'];
+    }
+
+    // Load values and assing defaults.
+    $header = get_field('header') ?: '';
+    $class_header_options = get_field('class_header_options') ?: '';
+    $class_header_size = get_field('class_header_size') ?: '';
+    $class_seo_header = get_field('class_seo_header') ?: '';
+    $margins = get_field('margins') ?: '';
+
+?>
+
+
+<div class="b-header -wp-block <?php echo esc_attr($className); ?> <?php echo esc_attr($margins); ?> <?php echo esc_attr($class_header_options); ?> <?php echo esc_attr($class_header_size); ?>">
+    <<?php echo esc_attr($class_seo_header); ?>><?php echo $header; ?></<?php echo esc_attr($class_seo_header); ?>>
+</div>
