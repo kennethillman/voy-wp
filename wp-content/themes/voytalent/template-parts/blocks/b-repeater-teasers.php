02669607e2155e6b13f5391dd00605b96ff0f91d
Kenneth Illman <kenneth.illman@gmail.com>
2019-05-20 18:32:49 +0530

    .

--- wp-content/themes/voytalent/template-parts/blocks/b-repeater-teasers.php
+++ wp-content/themes/voytalent/template-parts/blocks/b-repeater-teasers.php
@@ -18,24 +18,28 @@
 ?>
 
 <div class="b-repeater-teasers <?php echo esc_attr($className); ?> <?php echo esc_attr($class_margins_options_); ?>">
-    <h3 class="special-header -border-bottom"><?php echo $header;?></h3>
+  <div class="gc">
+    <div class="g-12">
+      <h3 class="special-header"><?php echo $header;?></h3>
 
-    <?php
-        foreach ($teaser as $rt):
-    ?>
-        <div class="g-4 g-t-6 g-4 g-m-12">
-            <a href="<?php echo $rt['link']['url']; ?>" class="b-repeater-teaser">
-                <figure>
-                    <img src="<?php echo $rt['image']['size']['medium']; ?>" />
-                </figure>
-                <div class="text">
-                    <h4><?php echo $rt['header']; ?></h4>
-                    <p><?php echo $rt['text']; ?></p>
-                </div>
-            </a>
-        </div>
+      <?php
+          foreach ($teaser as $rt):
+      ?>
+          <div class="g-4 g-t-6 g-4 g-m-12">
+              <a href="<?php echo $rt['link']['url']; ?>" class="b-repeater-teaser">
+                  <figure>
+                      <img src="<?php echo $rt['image']['size']['medium']; ?>" />
+                  </figure>
+                  <div class="text">
+                      <h4><?php echo $rt['header']; ?></h4>
+                      <p><?php echo $rt['text']; ?></p>
+                  </div>
+              </a>
+          </div>
 
-    <?php
-        endforeach;
-    ?>
+      <?php
+          endforeach;
+      ?>
+    </div>
+  </div>
 </div>
