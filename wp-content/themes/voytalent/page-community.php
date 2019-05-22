d68ea737bb2e7f138d1f3d3cf0a5f1a31075c5ff
Kavitha <kavitha.cm@indpro.se>
2019-05-22 10:24:17 +0530

    widgets

---
+++ wp-content/themes/voytalent/page-community.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * Template Name: Community Page
+ * */
+get_header();
+?>
+
+<?php get_template_part('parts/s-featured-image'); ?>
+<?php get_template_part('parts/s-breadcrumbs'); ?>
+
+<article class="s- content p-stories">
+    <?php get_template_part('parts/s-no-featured-image'); ?>
+
+    <div class="gc" >
+        <div class="g-8 g-push-2 g-m-12 g-m-push-0 g-t-10 g-t-push-1" >
+            <?php
+                while (have_posts()) :
+                    the_post();
+                    the_content();
+                endwhile;
+            ?>
+        </div>
+    </div>
+
+    <?php
+        if ( is_active_sidebar( 'voy-sidebar-1' ) ) :
+            dynamic_sidebar( 'voy-sidebar-1' );
+        endif;
+    ?>
+
+</article>
+
+<?php
+get_footer();
+?>
