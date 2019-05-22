5185b60b1690adaec6c11a71a4a2d56ec33e4068
Kenneth Illman <kenneth.illman@gmail.com>
2019-05-22 09:09:45 +0530

    Removed join -> should be added in conent

--- wp-content/themes/voytalent/footer.php
+++ wp-content/themes/voytalent/footer.php
@@ -1,7 +1,7 @@
 
 
 
-<?php get_template_part( 'parts/s-join-email' ); ?>
+<!-- <?php get_template_part( 'parts/s-join-email' ); ?> -->
 <?php get_template_part( 'parts/s-follow' ); ?>
 <?php get_template_part( 'parts/s-footer' ); ?>
 
