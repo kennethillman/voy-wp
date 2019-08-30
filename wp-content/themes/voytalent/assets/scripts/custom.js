3d3971ac69f22433158d5fcda4024b73be5354ff
Kavitha <kavitha.cm@indpro.se>
2019-08-30 16:16:54 +0530

    form submitting result msg display

--- wp-content/themes/voytalent/assets/scripts/custom.js
+++ wp-content/themes/voytalent/assets/scripts/custom.js
@@ -28,20 +28,15 @@ function post_candidates() {
         if (this.status >= 200 && this.status < 400) {
             // If successful
             var result = JSON.parse(this.response);
-            var resMsg = '';
+            console.log("API Response ",result);
 
-            if(result.hasOwnProperty('status') && result.status == "created"){
-                resMsg = '<span style="color:green; font-weight: bold;">Submitted Successfully</span>';
-            }
-            if(result.hasOwnProperty('error')){
-                resMsg = '<span style="color:red; font-weight: bold;">'+result.error+'</span>';
-            }
-            document.getElementById("showPostCandidateResult").innerHTML = resMsg;
+            document.getElementById("showPostCandidateResult").style.display = "block";
+            showResult(result, false);
             document.getElementById("submitCandidate").disabled = false;
 
-            setTimeout(function() {
+            /*setTimeout(function() {
                 document.getElementById("showPostCandidateResult").style.display = "none";
-            }, 2500);
+            }, 2500);*/
         } else {
             // If fail
             console.log(this.response);
@@ -76,20 +71,13 @@ function submit_contact(){
     xhr.onload = function () {
         if (this.status >= 200 && this.status < 400) {
             var result = JSON.parse(this.response);
-            var resMsg = '';
-            console.log("API RESPONSE ",result);
-
-            if(result.hasOwnProperty('status') && result.status == "success"){
-                resMsg = '<span style="color:green; font-weight: bold;">'+result.msg+'</span>';
-            }
-            if(result.hasOwnProperty('error')){
-                resMsg = '<span style="color:red; font-weight: bold;">'+result.msg+'</span>';
-            }
-            document.getElementById("contactSubmitResult").innerHTML = resMsg;
+            console.log("Email Response ",result);
+            document.getElementById("contactSubmitResult").style.display = "block";
+            showResult(result, true);
             document.getElementById("send_email").disabled = false;
-            setTimeout(function() {
+            /*setTimeout(function() {
                 document.getElementById("contactSubmitResult").style.display = "none";
-            }, 2500);
+            }, 2500);*/
         } else {
             console.log(this.response);
         }
@@ -99,4 +87,20 @@ function submit_contact(){
     };
     xhr.send(actionData);
     return false;
+}
+
+function showResult(result, isContactForm) {
+    if(result.hasOwnProperty('status') && (result.status == "success" || result.status == "created")){
+        document.getElementById("result_sent").style.display = "block";
+        document.getElementById("result_error").style.display = "none";
+    }
+    else if(result.hasOwnProperty('status')  && result.status == "error" && isContactForm){
+        document.getElementById("result_sent").style.display = "none";
+        document.getElementById("result_error").style.display = "block";
+    }
+    else if(!isContactForm && result.error != "undefined"){
+        document.getElementById("result_sent").style.display = "none";
+        document.getElementById("result_error").innerHTML = '<h2 class="header">' + result.error + '</h2>';
+        document.getElementById("result_error").style.display = "block";
+    }
 }
--- wp-content/themes/voytalent/page-contact.php
+++ wp-content/themes/voytalent/page-contact.php
@@ -58,7 +58,14 @@
                             <div class="btn-holder">
                                 <button type="submit" class="btn -yellow -icon" id="send_email">Send<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 3v18h24v-18h-24zm21.518 2l-9.518 7.713-9.518-7.713h19.036zm-19.518 14v-11.817l10 8.104 10-8.104v11.817h-20z"/></svg></button>
                             </div>
-                            <div id="contactSubmitResult"></div>
+                            <div id="contactSubmitResult" style="display: none">
+                                <div class="sent-result -sent" id="result_sent">
+                                    <h2 class="header">Sent! Thank you!</h2>
+                                </div>
+                                <div class="sent-result -error" id="result_error">
+                                    <h2 class="header">Something went wrong, sorry!</h2>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </form>
