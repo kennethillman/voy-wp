28ae36b7a66764cfea998f8d5bd542950021d55a
Kenneth Illman <kenneth.illman@gmail.com>
2019-08-30 14:23:56 +0530

    added conditions

--- wp-content/themes/voytalent/parts/jobs/job-details.php
+++ wp-content/themes/voytalent/parts/jobs/job-details.php
@@ -23,15 +23,18 @@ global $jobID;
         <span class="text"><?php echo (isset($jobDetails->function)?$jobDetails->function.', ':''); echo $jobDetails->title; ?></span>
     </div>
 
-    <?php if($jobID != "DF3F17BC4C") : ?> <!-- OR : Hide if empty -->
+    <?php if($jobDetails->employment_type != "") : ?> <!-- OR : Hide if empty -->
       <div class="detail">
         <span class="header">Employment:</span>
-        <span class="text"><?php echo $jobDetails->employment_type; ?></span>
+        <span class="text"><?php echo $jobDetails->employment_type ?></span>
       </div>
     <?php endif; ?>
 
-    <div class="detail"> <!-- Hide if empty -->
-        <span class="header">Location:</span>
-        <span class="text"><?php echo (isset($jobDetails->location->region)?$jobDetails->location->region.', ':''); echo $jobDetails->location->country; ?></span>
-    </div>
+    <?php if($jobDetails->location->country != "") : ?> <!-- OR : Hide if empty -->
+      <div class="detail"> <!-- Hide if empty -->
+          <span class="header">Location:</span>
+          <span class="text"><?php echo (isset($jobDetails->location->region)?$jobDetails->location->region.', ':''); echo $jobDetails->location->country; ?></span>
+      </div>
+    <?php endif; ?>
+
 </div>
