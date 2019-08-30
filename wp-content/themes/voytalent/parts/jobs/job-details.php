5378485cef010b33dd63ee4e7ef8f5e4e0e9ffa0
Kenneth Illman <kenneth.illman@gmail.com>
2019-08-30 10:17:09 +0530

    added conditionals, start

--- wp-content/themes/voytalent/parts/jobs/job-details.php
+++ wp-content/themes/voytalent/parts/jobs/job-details.php
@@ -8,18 +8,29 @@
  * @version 1.0
  */
 global $jobDetails;
+global $jobID;
 ?>
 
 <div class="job-details -pattern-stripe-light">
     <div class="detail">
-        <span class="header">Job:</span>
+
+        <?php if($jobID != "DF3F17BC4C") { ?>
+            <span class="header">Job: </span>
+        <?php } else { ?>
+            <span class="header">Join: </span>
+        <?php } ?>
+
         <span class="text"><?php echo (isset($jobDetails->function)?$jobDetails->function.', ':''); echo $jobDetails->title; ?></span>
     </div>
-    <div class="detail">
+
+    <?php if($jobID != "DF3F17BC4C") : ?> <!-- OR : Hide if empty -->
+      <div class="detail">
         <span class="header">Employment:</span>
         <span class="text"><?php echo $jobDetails->employment_type; ?></span>
-    </div>
-    <div class="detail">
+      </div>
+    <?php endif; ?>
+
+    <div class="detail"> <!-- Hide if empty -->
         <span class="header">Location:</span>
         <span class="text"><?php echo (isset($jobDetails->location->region)?$jobDetails->location->region.', ':''); echo $jobDetails->location->country; ?></span>
     </div>
