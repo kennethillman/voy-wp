f18c993d9bf11eef44855df4882f6afda806c859
Pradipta Barik <prad.brk@gmail.com>
2019-10-16 17:07:42 +0530

    Form validation fixed

--- wp-content/themes/voytalent/parts/s-join-voy.php
+++ wp-content/themes/voytalent/parts/s-join-voy.php
@@ -16,7 +16,7 @@
             'UX Designer / Visual Designer', 'Video', 'Video & photography', 'Web design', 'Web Developer'
         );
     ?>
-    <form id="postCandidates" name="postCandidates" method="post" enctype="multipart/form-data">
+    <form id="postCandidates" name="postCandidates" method="post" enctype="multipart/form-data" onsubmit="return post_candidates(event)">
         <div class="gc">
           <div class="g-12">
                 <div class="component-divider">
@@ -118,7 +118,7 @@
 
         <div class="g-12">
           <br>
-            <button type="submit" class="btn -orange -icon" id="submitCandidate" onClick="post_candidates();">Send<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 3v18h24v-18h-24zm21.518 2l-9.518 7.713-9.518-7.713h19.036zm-19.518 14v-11.817l10 8.104 10-8.104v11.817h-20z"></path></svg></button>
+            <button type="submit" class="btn -orange -icon" id="submitCandidate" >Send<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 3v18h24v-18h-24zm21.518 2l-9.518 7.713-9.518-7.713h19.036zm-19.518 14v-11.817l10 8.104 10-8.104v11.817h-20z"></path></svg></button>
         </div>
     </form>
 
